5bf81b90b54dd7a56138f5f9642404475f77c1c8
Tom Deseyn <tom.deseyn@gmail.com>
2021-05-06 12:20:35 +0200

    new console app

---
+++ src/linux-dev-certs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace linux_dev_certs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
